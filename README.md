47db79b3e1b4e723ac5c55c32e69e5a24b3aebe3
clivefang <clivefang@tencent.com>
2019-12-13 18:43:59 +0800

    Initialize

---
+++ README.md
@@ -0,0 +1,7 @@
+# keras_bert_ner
+基于keras和keras_bert的中文命名实体识别，搭建的网络为bert+bilstm_crf
+运行main函数即可训练并使用模型
+environment:
+Keras version:2.2.4
+keras-contrib version:2.0.8
+需要下载中文预训练模型chinese_L-12_H-768_A-12放到Parameter文件夹下
---
+++ main.py
@@ -0,0 +1,40 @@
+from Model.BertBilstmCrf import bert_bilstm_crf
+from Model import DealWithData
+import json
+
+
+if __name__ == "__main__":
+    in_web = True
+    # 数据
+    if not in_web:
+        train_path = "./Data/train1.txt"
+        test_path = "./Data/test.txt"
+        train_data = DealWithData.PreProcessData(train_path)
+        test_data = DealWithData.PreProcessData(test_path)
+    else:
+        train_path = "./Data/train.json"
+        test_path = "./Data/test.json"
+        train_data = []
+        test_data = []
+        with open(train_path, "r", encoding="utf-8") as f:
+            for line in f.readlines():
+                dic = json.loads(line)
+                train_data.append(dic)
+        with open(test_path, "r", encoding="utf-8") as f:
+            for line in f.readlines():
+                dic = json.loads(line)
+                test_data.append(dic)
+
+    # 模型
+    max_seq_length = 80
+    batch_size = 24
+    epochs = 100
+    lstmDim = 64
+    model = bert_bilstm_crf(max_seq_length, batch_size, epochs, lstmDim)
+    model.TrainModel(train_data, test_data)
+
+    # 测试
+    while 1:
+        sentence = input('please input sentence:\n')
+        tag = model.ModelPredict(sentence)
+        print(tag)
