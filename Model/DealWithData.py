ebf079fb945d61a78f9d32c53ad28cbfd915ec93
jxq96 <jxq96@mail.ustc.edu.cn>
2019-12-14 15:21:26 +0800

    finish Generate Data

--- Model/DealWithData.py
+++ Model/DealWithData.py
@@ -21,10 +21,16 @@ def PreProcessData(path):
 
 def GenerateData(json_path, train_path, validate_path):
     datas = ["", ""]  # first for training data, second for validate data
-    tag_map = {'疾病和诊断': 1, '影像检查': 2, '实验室检验': 3, '药物': 4, '手术': 5, '解剖部位': 6}
+    tag_map = {'疾病和诊断': 4, '影像检查': 2, '实验室检验': 1, '药物': 5, '手术': 3, '解剖部位': 6}
+    _max_sentence = 0
+    num = 0
+    label_list = ['B-LAB', 'I-LAB', 'B-RAY', 'I-RAY', 'B-OPE', 'I-OPE', 'B-DIS', 'I-DIS', 'B-MED',
+                  'I-MED', 'B-ANA', 'I-ANA']
     count = 0
     with open(json_path, encoding='utf-8') as f:
         for line in f:  # every line is a json object
+            start_sentence = 0
+            sentence_cur = 0
             ini_pos = 0
             count += 1
             element = json.loads(line, encoding='utf-8')
@@ -39,33 +45,59 @@ def GenerateData(json_path, train_path, validate_path):
                 end_pos = entity['end_pos']
                 tag = tag_map[entity['label_type']]
                 for i in range(ini_pos, start_pos):
+                    sentence_cur += 1
                     datas[choose_index] += original_text[i]
                     datas[choose_index] += ' 0\n'
-                    if original_text[i] == '。':
+                    if original_text[i] == '。' or original_text[i] == '，' or original_text[i] == ',' or original_text[i] == '.':
                         datas[choose_index] += '\n'
+                        if sentence_cur - start_sentence >= 80:
+                            num += 1
+                            _max_sentence = sentence_cur - start_sentence
+                            print(_max_sentence)
+                            print(original_text[start_sentence:sentence_cur])
+                        start_sentence = sentence_cur
                 datas[choose_index] += original_text[start_pos]
                 datas[choose_index] += ' '
-                datas[choose_index] += str(2*tag-1)
+                datas[choose_index] += label_list[2*tag-1]
                 datas[choose_index] += '\n'
-                tmpstr = ' ' + str(2*tag) + '\n'
+                tmpstr = ' ' + label_list[2*tag] + '\n'
+                sentence_cur += 1
                 for i in range(start_pos+1, end_pos):
                     datas[choose_index] += original_text[i]
                     datas[choose_index] += tmpstr
+                    sentence_cur += 1
+                    if original_text[i] == '。' or original_text[i] == '，'or original_text[i] == ',' or original_text[i] == '.':
+                        datas[choose_index] += '\n'
+                        if sentence_cur - start_sentence >= 80:
+                            num += 1
+                            _max_sentence = sentence_cur - start_sentence
+                            print(_max_sentence)
+                            print(original_text[start_sentence:sentence_cur])
+                        start_sentence = sentence_cur
                 ini_pos = end_pos
             for i in range(end_pos, len(original_text)):
                 datas[choose_index] += original_text[i]
                 datas[choose_index] += ' 0\n'
-                if original_text[i] == '。':
+                if original_text[i] == '。' or original_text[i] == '，'or original_text[i] == ',' or original_text[i] == '.':
                     datas[choose_index] += '\n'
+                    if sentence_cur - start_sentence >= 80:
+                        num += 1
+                        _max_sentence = sentence_cur - start_sentence
+                        print(_max_sentence)
+                        print(original_text[start_sentence:sentence_cur])
+                    start_sentence = sentence_cur
     with open(train_path, encoding='utf-8', mode='w') as f:
         f.write(datas[0])
     with open(validate_path, encoding='utf-8', mode='w') as f:
         f.write(datas[1])
+    print("num: ", num)
+    return _max_sentence
+
 
 # test GenerateData
-
 if __name__ == '__main__':
-    GenerateData('../Data/train.json', '../Data/train.txt', '../Data/validate.txt')
+    max_sentence = GenerateData('../Data/train.json', '../Data/train.txt', '../Data/validate.txt')
+    print(max_sentence)
 
 
 
